ebdde9cf4adc42c9030333b03dd92864adae72b8
unknown <jrinho22@gmail.com>
2025-02-20 23:22:02 -0300

    rebase commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { RouterProvider } from "react-router-dom"
-import router from "./router/router"
 import { Provider } from "react-redux";
 import storeInstance from "./store/store";
 import Router from "./router/router";
 
 function App() {
 
-  // useEffect(() => {
-  //   console.log('Component re-rendered');
-  // }, []);
   return (
     <Provider store={storeInstance}>
       <Router />
